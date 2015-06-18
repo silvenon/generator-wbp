7793edb904aaf3eb024cd2be6063d3788b6542f6
Matija Marohnić <matija.marohnic@gmail.com>
2015-06-18 14:01:25 +0200

    Update to yeoman-generator v0.20.x

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,9 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = generators.Base.extend({
   prompting: function () {
     var done = this.async();
 
