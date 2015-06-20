b31d9494eac440ca40ff7a4eb0bf6415de214ae7
Matija Marohnić <matija.marohnic@gmail.com>
2015-06-21 00:01:02 +0200

    Add Fetch API example to the non-React version too
    Ref #4.

--- generators/app/templates/app/scripts/app.js
+++ generators/app/templates/app/scripts/app.js
@@ -1,4 +1,10 @@
+import fetch from './helpers/fetch';
+
 import 'svg4everybody';
 import './fonts';
 
 console.log('Hello World');
+
+fetch('/index.html').then((res) => {
+  console.log('Fetch API call successful!');
+});
