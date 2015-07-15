eb47cd6c2fb329ca649e0f671d0312914fa836d2
Matija Marohnić <matija.marohnic@gmail.com>
2015-07-15 15:55:37 +0200

    Imbue the fetch helper with status powers

--- generators/app/templates/scripts/helpers/fetch.js
+++ generators/app/templates/scripts/helpers/fetch.js
@@ -1,6 +1,15 @@
 import 'es6-promise';
 import 'fetch';
 
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
 export default (url, options) => {
-  return fetch(url, options);
+  return fetch(url, options)
+    .then(status);
 };
