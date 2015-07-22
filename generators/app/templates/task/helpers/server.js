f09815393300cdb3406a797c850dc00acdc4b174
Matija Marohnić <matija.marohnic@gmail.com>
2015-07-22 23:01:18 +0200

    Add test server only when React is off

--- generators/app/templates/task/helpers/server.js
+++ generators/app/templates/task/helpers/server.js
@@ -1,5 +1,7 @@
 import browserSync from 'browser-sync';
 
 export const dev = browserSync.create('dev');
+<% if (!includeReact) { -%>
 export const test = browserSync.create('test');
+<% } -%>
 export const prod = browserSync.create('prod');
